9a53a9dc55a41db904fe75fa398612cb92bb2833
Cooper Harris <cwharris365@gmail.com>
2024-03-12 20:38:58 -0700

    Initial Commit

---
+++ LittleLemonFinal/LittleLemonFinalApp.swift
@@ -0,0 +1,20 @@
+//
+//  LittleLemonFinalApp.swift
+//  LittleLemonFinal
+//
+//  Created by Cooper Harris on 3/12/24.
+//
+
+import SwiftUI
+
+@main
+struct LittleLemonFinalApp: App {
+    let persistenceController = PersistenceController.shared
+
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+                .environment(\.managedObjectContext, persistenceController.container.viewContext)
+        }
+    }
+}
