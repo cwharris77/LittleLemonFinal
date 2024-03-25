01723a95a164e67f5ee8b5c40ff7e80ad4fef2ae
Cooper Harris <cwharris365@gmail.com>
2024-03-25 13:21:15 -0700

    Updated menu view to fetch data from remote json file, added a serch box to filter results

---
+++ LittleLemonFinal/MenuItem.swift
@@ -0,0 +1,8 @@
+//
+//  MenuItem.swift
+//  LittleLemonFinal
+//
+//  Created by Cooper Harris on 3/25/24.
+//
+
+import Foundation
---
+++ LittleLemonFinal/MenuList.swift
@@ -0,0 +1,8 @@
+//
+//  MenuList.swift
+//  LittleLemonFinal
+//
+//  Created by Cooper Harris on 3/25/24.
+//
+
+import Foundation
